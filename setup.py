ce6deb0abddc5a1b4f8bd99268b598028ac0a1d7
Ophir Neto <ophirn@icloud.com>
2019-01-16 02:54:49 -0200

    I don't think this changed much

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(
-    ext_modules = cythonize("cannon.pyx")
-)
+            ext_modules = cythonize(["cannon.pyx"],annotate=True)
+ )
