74be976959de1fba03ed114e93c4d0ae1c4211a2
Ophir Neto <ophir@Ophirs-MacBook-Air.local>
2019-01-22 21:55:27 -0200

    Now with pointers

--- cannon.c
+++ cannon.c
@@ -9,7 +9,8 @@ typedef int bool;
 
 /* System constants & Debug miscellania */
 
-#define SCALE_FACTOR 0.005
+#define SCALE_FACTOR 0.001
+#define CIRC_SCALE_FACTOR 0.005
 #define DEBUG false
 
 
@@ -33,9 +34,9 @@ struct Circle {
 
 
 // Returns wether or not the point (x,y) belongs to the circle.
-bool belongsTo(double x, double y, double x_0, double y_0, double radius){
+bool belongsTo(double *x, double *y){
 
-	return (x - x_0)*(x - x_0) + (y - y_0)*(y - y_0) <= radius*radius;  
+	return (*x - fortress.x_0)*(*x - fortress.x_0) + (*y - fortress.y_0)*(*y - fortress.y_0) <= fortress.radius*fortress.radius;  
 
 }
 
@@ -54,7 +55,7 @@ struct LineOfSight {
 double getAngle(double x, double y){
 	
 	double angle;
-	angle = acos( x/( pow( (x*x + y*y),0.5) ) );
+	angle = acos( ( x/( pow( x * x + y * y,0.5) ) ) );
 
 	if(x < 0 && y < 0) { return (2*(M_PI - angle) + angle); } //Third quad.
 	else if(x > 0 && y < 0) { return (2*M_PI - angle); } //Fourth quad.
@@ -64,12 +65,12 @@ double getAngle(double x, double y){
 
 
 // Determines if the point is within the area of sight of the cannon, or not
-bool isHit(double x, double y, double x_0, double y_0, double angle, double spread){
+bool isHit(double *x, double *y){
 	
-	double vector_angle = getAngle(x - x_0, y - y_0);
-	double hspread = spread/2; // stores half the spread value for performance reasons
+	double vector_angle = getAngle( (*x) - cannon.x_0, (*y) - cannon.y_0);
+	double hspread = cannon.spread/2; // stores half the spread value for performance reasons
 
-	return (vector_angle >= angle - hspread && vector_angle <= angle + hspread);
+	return (vector_angle >= cannon.angle - hspread && vector_angle <= cannon.angle + hspread);
 }
 
 
@@ -82,56 +83,7 @@ struct Iteration {
 	double x_i, y_i, x_begin, y_begin, x_end, y_end;
 	double length, area_grid, total_area;
 
-
-
 } iterate;
-
-struct IterateCircle{
-
-	double total_area, dangle, dradius, area_grid;
-	double angle;
-	
-
-
-
-
-} itcirc;
-
-
-// Iterates over a circle (i.e. the fortress itself)
-
-double getx(){ return (fortress.x_0 + fortress.radius * cos(itcirc.angle)); }
-
-double gety() { return (fortress.y_0 + fortress.radius * sin(itcirc.angle)); }
-
-void scan_circle(){
-
-	itcirc.dangle = 5*SCALE_FACTOR;
-	itcirc.dradius = fortress.radius * SCALE_FACTOR;
-	itcirc.area_grid = itcirc.dradius * itcirc.dangle * fortress.radius;
-	itcirc.total_area = 0;
-
-	
-	while(fortress.radius > 0){
-		
-		while(itcirc.angle < 2*M_PI){
-		
-			if(isHit(getx(), gety(), cannon.x_0, cannon.y_0, cannon.angle, cannon.spread)) { itcirc.total_area += itcirc.area_grid; }
-			
-			itcirc.angle += itcirc.dangle;
-
-		}
-
-		fortress.radius -= itcirc.dradius;
-		itcirc.angle = 0;
-		itcirc.area_grid = itcirc.dradius * itcirc.dangle * fortress.radius;
-
-	}
-
-	printf("%.1lf\n", itcirc.total_area);
-
-		
-}
 
 
 // Iterates over a circunscribed square in relation to the fortress's circle
@@ -155,7 +107,7 @@ void scan(){
 
 			if(DEBUG) { printf("y: %lf x: %lf", iterate.y_i, iterate.x_i); }
 
-			if( belongsTo(iterate.x_i, iterate.y_i, fortress.x_0, fortress.y_0, fortress.radius) && isHit (iterate.x_i, iterate.y_i, cannon.x_0, cannon.y_0, cannon.angle, cannon.spread) ) {
+			if( belongsTo( &(iterate.x_i), &(iterate.y_i) ) && isHit ( &(iterate.x_i), &(iterate.y_i) ) ) {
 				iterate.total_area += iterate.area_grid;
 				if(DEBUG) { printf("Total area so far: %lf", iterate.total_area); }
 			}
@@ -170,6 +122,52 @@ void scan(){
 
 }
 
+
+/*
+ // Iterates over a circle (i.e. the fortress itself)
+ 
+ struct IterateCircle{
+
+	double total_area, dangle, dradius, area_grid;
+	double angle;
+
+} itcirc;
+
+
+double getx(){ return (fortress.x_0 + fortress.radius * cos(itcirc.angle)); }
+
+double gety() { return (fortress.y_0 + fortress.radius * sin(itcirc.angle)); }
+
+void scan_circle(){
+
+	itcirc.dangle = 5*CIRC_SCALE_FACTOR;
+	itcirc.dradius = fortress.radius * CIRC_SCALE_FACTOR;
+	itcirc.area_grid = itcirc.dradius * itcirc.dangle * fortress.radius;
+	itcirc.total_area = 0;
+
+	
+	while(fortress.radius > 0){
+		
+		while(itcirc.angle < 2*M_PI){
+		
+			if(isHit(getx(), gety()) { itcirc.total_area += itcirc.area_grid; }
+			
+			itcirc.angle += itcirc.dangle;
+
+		}
+
+		fortress.radius -= itcirc.dradius;
+		itcirc.angle = 0;
+		itcirc.area_grid = itcirc.dradius * itcirc.dangle * fortress.radius;
+
+	}
+
+	printf("%.1lf\n", itcirc.total_area);
+
+		
+}
+
+*/
 
 
 
